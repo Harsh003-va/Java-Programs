6ca76dbd081066c58ec380f9c5c6c28eb87132a7
Harshavardhan Atigeri <72183253+Harsh003-va@users.noreply.github.com>
2023-12-07 10:47:42 +0530

    mathClass

---
+++ mathClass.java
@@ -0,0 +1,18 @@
+public class mathClass {
+    public static void main(String arg[])
+    {
+        double x=20;
+        double y=18;
+
+        System.out.println("Maximum number of x and y is: "+ Math.max(x,y));
+
+        System.out.println("sqauare root of y is :"+ Math.sqrt(y));
+
+        System.out.println("Power of x and y is :"+ Math.pow(x, y));
+
+        System.out.println("Minimum of x and y is :"+Math.min(x, y));
+
+        System.out.println("Absolute value of y :" + Math.abs(y));
+    }
+    
+}
