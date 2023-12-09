e6908df87c4f0732418344baf7e75cc66d8b5ce9
Harshavardhan Atigeri <72183253+Harsh003-va@users.noreply.github.com>
2023-12-09 15:17:37 +0530

    java

--- ButterflyPattern.java
+++ ButterflyPattern.java
@@ -2,16 +2,85 @@ public class ButterflyPattern {
 
 public static void butterflyPattern(int n){
     for (int i=1;i<=n; i++){
+
+        //Stars
         for(int j=1;j<=i;j++){
-            
+            System.out.print("*");
         }
+        //Sapces 
+        for(int j=1;j<=2*(n-i);j++){
+            System.out.print(" ");
+        }
+        //Stars
+      for(int j=1;j<=i;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+}
+//2nd half 
+for(int i=n;i>=1;i--){
+     for(int j=1;j<=i;j++){
+            System.out.print("*");
+        }
+        //Sapces 
+        for(int j=1;j<=2*(n-i);j++){
+            System.out.print(" ");
+        }
+        //Stars
+      for(int j=1;j<=i;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
+
+public static void solidRhombus(int n){
+    for (int i=1;i<=n;i++){
+        //spaces
+        for(int j=1;j<=n-i;j++){
+
+            System.out.print(" ");
+        }
+        //Stars
+
+        for(int j=1;j<=n;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
+
+
+//Hollow Rhombus
+public static void hollowRhombus(int n){
+    for(int i=1;i<=n;i++){
+        //spaces
+        for (int j=1;j<=(n-i);j++){
+            System.out.print(" ");
+        }
+        //hollow rectangle
+         for (int j=1;j<=n;j++){
+            if (i==1||i==n || j==1 || j==n){
+                System.out.print("*");
+            }else{
+                System.out.print(" ");
+            }
+           
+         }
+          System.out.println();
     }
 }
 
 
     public static void main(String arg[])
     {
-
-
+System.out.println("Pattern for Butterfly !");
+butterflyPattern(4);
+System.out.println();
+System.out.println("Pattern for Rhombus !");
+solidRhombus(4);
+System.out.println();
+System.out.println("Pattern for hollow Rhombus !");
+hollowRhombus(4);
     }
 }
