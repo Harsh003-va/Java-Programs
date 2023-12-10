4ba4374ea5850ffc41b4ce59069a4150c55dcc99
Harshavardhan Atigeri <72183253+Harsh003-va@users.noreply.github.com>
2023-12-10 10:20:33 +0530

    java

--- ButterflyPattern.java
+++ ButterflyPattern.java
@@ -71,16 +71,53 @@ public static void hollowRhombus(int n){
     }
 }
 
+public static void diamond(int n)
+{
+   for (int i=1; i<=n;i++)
+{
+    //spaces
+
+    for(int j=1;j<=n-i;j++){
+        System.out.print(" ");
+    }
+    //stars
+    for(int j=1;j<=(2*i-1) ;j++){
+        System.out.print("*");
+
+    }
+    System.out.println();
+}
+//2nd half
+for(int i=n;i>=1;i--){
+     //spaces
+
+    for(int j=1;j<=n-i;j++){
+        System.out.print(" ");
+    }
+    //stars
+    for(int j=1;j<=(2*i-1) ;j++){
+        System.out.print("*");
+
+    }
+    System.out.println();
+}
+
+}
 
     public static void main(String arg[])
     {
 System.out.println("Pattern for Butterfly !");
 butterflyPattern(4);
+
 System.out.println();
 System.out.println("Pattern for Rhombus !");
 solidRhombus(4);
+
 System.out.println();
 System.out.println("Pattern for hollow Rhombus !");
 hollowRhombus(4);
+System.out.println("Pattern for Diamond !");
+System.out.println();
+diamond(4);
     }
 }
