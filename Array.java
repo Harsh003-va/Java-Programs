d249564e9412ac71300a9d95ab30ff7207ce745b
Harshavardhan Atigeri <72183253+Harsh003-va@users.noreply.github.com>
2023-12-11 11:34:47 +0530

    java

---
+++ Array.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+public class Array {
+    public static void main(String arg[]){
+int marks[]= new int[100];
+Scanner sc =new Scanner(System.in);
+
+System.out.println("length of marks array = " + marks.length);
+// marks[0]=sc.nextInt();
+// marks[1]=sc.nextInt();
+// marks[2]=sc.nextInt();
+// System.out.println("phy : "+ marks[0]);
+// System.out.println("chem : "+ marks[1]);
+// System.out.println("math: "+ marks[2]);
+// int percentage = (marks[0]+marks[1]+marks[2])/3;
+// System.out.println("Percentage = "+ percentage +"%");
+    }
+}
