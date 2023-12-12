89c56a8f47e17a27a0eb0a36a31475a92efbf497
Harshavardhan Atigeri <72183253+Harsh003-va@users.noreply.github.com>
2023-12-12 20:57:08 +0530

    update

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,9 +1,29 @@
 public class BinarySearch {
 
-    public static int BinaryS(int number[]){
-        
+    public static int Binary(int numbers[],int key){
+        int start=0;
+        int end =numbers.length-1;
+
+        while(start <=end){
+            int mid = (start+end)/2;
+
+            //comparisons
+            if(numbers[mid] ==key){
+                return mid;
+            }
+            if(numbers[mid]<key){ //right
+                start =mid +1;
+
+            }else{
+                end = mid -1;
+            }
+
+        }
+        return -1;
     }
     public static void main(String arg[]){
-
+      int numbers[]={2,4,6,8,10,12,14};
+      int key=10;
+      System.out.println("index for key is : "+Binary(numbers, key));
     }
 }
