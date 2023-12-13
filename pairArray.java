62d532d72dadcead0a69fc7772e488801450baf2
Harshavardhan Atigeri <72183253+Harsh003-va@users.noreply.github.com>
2023-12-13 16:03:26 +0530

    update

--- pairArray.java
+++ pairArray.java
@@ -1,12 +1,15 @@
 public class pairArray {
     public static void printPairs(int numbers[]){
+        int tp=0;
         for(int i=0;i<numbers.length;i++){
             int curr = numbers[i];
             for(int j=i+1;j<numbers.length;j++){
                 System.out.print("(" + curr+","+ numbers[j]+")");
+                tp++;
             }
             System.out.println();
         }
+        System.out.println("Total pairs = "+tp);
     }
     public static void main(String arg[]){
         int numbers[]={2,4,6,8,10};
