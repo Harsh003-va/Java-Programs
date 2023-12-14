2ffa2e1f00d5a1b9e872d19092c0484f48eb16a9
Harshavardhan Atigeri <72183253+Harsh003-va@users.noreply.github.com>
2023-12-14 11:39:55 +0530

    java

--- printSubarray.java
+++ printSubarray.java
@@ -1,28 +1,34 @@
+import java.util.*;
+
 public class printSubarray {
 
-    public static void subarray(int numbers[])
+   
+    public static void MaxsubarraySum(int numbers[])
     {
-        int sum=0;
-        int ts=0;
+        int currSum=0;
+        int maxSum=Integer.MIN_VALUE;
+      
        for(int i=0;i<numbers.length;i++){
         int start = i;
         for(int j=i;j<numbers.length;j++){
            int end=j;
+           currSum=0;
            for(int k=start;k<=end;k++){
-            System.out.print(numbers[k]+" ");
+           currSum+=numbers[k];
              
            }
+           System.out.println(currSum);
+           if(maxSum<currSum){
+            maxSum=currSum;
+           }
           
-           ts++;
-           System.out.println();
         }
-        System.out.println();
+        
        } 
-       System.out.println("Total subarrays = "+ts);
-    System.out.println(sum);
+     System.out.println("max sum = "+maxSum);
     }
     public static void main(String arg[]){
         int numbers[]={2,4,6,8,10};
-        subarray(numbers);
+        MaxsubarraySum(numbers);
     }
 }
