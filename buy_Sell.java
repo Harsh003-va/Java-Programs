e60d6c89d5fb84ce17077504ca451e1ff2784072
Harshavardhan Atigeri <72183253+Harsh003-va@users.noreply.github.com>
2023-12-21 21:40:05 +0530

    java

---
+++ buy_Sell.java
@@ -0,0 +1,22 @@
+public class buy_Sell {
+
+    public static int buyAndsellStocks(int prices[]){
+        int buyprice=Integer.MAX_VALUE;
+        int maxProfit=0;
+        for (int i=0;i<prices.length;i++)
+{
+if (buyprice< prices[i]){//profit
+    int profit = prices[i] - buyprice; // today's profit
+    maxProfit =Math.max(maxProfit,profit);
+}else {
+    buyprice =prices[i];
+}
+
+} return maxProfit;
+    } 
+    public static void main(String arg[]){
+        int prices[]={7,1,5,3,6,4};
+        System.out.println(buyAndsellStocks(prices));
+    }
+    
+}
